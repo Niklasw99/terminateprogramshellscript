71f9f395de17719b2c97c3ebdb88c90bda37fd94
Niklasw99 <32064953+Niklasw99@users.noreply.github.com>
2024-05-22 22:39:04 +0200

    Update script.sh
    Fixed |p

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 killname=$1
-ps x | grep "$killname" | grep -o "[0-9]*[\ ][\?]" |   grep -o "[0-9]*" | xargs -I {} kill -9 {}
+ps x | grep "$killname" | grep -o "[0-9]*[\ ][\?|p]" |   grep -o "[0-9]*" | xargs -I {} kill -9 {}
